a0b6cfba596154cec8085e2d4ba90c1e56164799
ReduxMPX <72150451+ReduxMPX@users.noreply.github.com>
2022-01-23 18:42:40 -0800

    Create rdx-main.js

---
+++ rdx-main.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", preventMotion, false);
+window.addEventListener("touchmove", preventMotion, false);
+
+function preventMotion(event)
+{
+    window.scrollTo(0, 0);
+    event.preventDefault();
+    event.stopPropagation();
+}
+

53586e5b2b208d88aff7a1671ccd441dad924d8a
ReduxMPX <72150451+ReduxMPX@users.noreply.github.com>
2022-01-26 10:13:44 -0800

    Update home-main.js

--- home-main.js
+++ home-main.js
@@ -1,10 +1,1 @@
-console.log("RDX Home V0.3")
-
-
-import Toasted from 'toastedjs'
-
-import 'toastedjs/dist/toastedjs.min.css'  
-//import 'toastedjs/src/sass/toast.scss'
-
-let toasted = new Toasted({ /* your options.. */ })
-toasted.show('Loadcall returned - temp message, REDUXSYS')
+console.log("RDX Home V0.4")

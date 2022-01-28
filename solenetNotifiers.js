ddbb8e94d535063ef261d534204126507416c859
ReduxMPX <72150451+ReduxMPX@users.noreply.github.com>
2022-01-28 11:43:16 -0800

    Create solenetNotifiers.js

---
+++ solenetNotifiers.js
@@ -0,0 +1,9 @@
+const solenetFacts = ["💜 SoleNet is run by one person!",
+                      "📹 You should follow us on TikTok @Solenet!",
+                      "📱 SoleNet hosts livestreams!",
+                      "📍 SoleNet is based in LA!",
+                      "📊 Our analytics are publicly visible!",
+                      "📱 Our site is built specifically for the Instagram browser!"]
+
+var chosenFact = items[Math.floor(Math.random()*solenetFacts.length)];
+console.log(chosenFact)

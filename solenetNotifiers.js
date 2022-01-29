51cff23be06141f1c50ddb7270c1ec023aa73128
ReduxMPX <ReduxMPX@gmail.com>
2022-01-28 23:48:55 -0800

    added facts

--- solenetNotifiers.js
+++ solenetNotifiers.js
@@ -7,11 +7,16 @@
 
 function showFact() {
   const solenetFacts = ["💜 SoleNet is run by one person!",
-                      "📹 You should follow us on TikTok @Solenet!",
+                      "📹 You should follow us on TikTok!",
                       "📱 SoleNet hosts livestreams!",
                       "📍 SoleNet is based in LA!",
                       "📊 Our analytics are publicly visible!",
-                      "📱 Our site is built specifically for the Instagram browser!"]
+                      "📱 Our site is built  Instagram!",
+                      "📦 Check out our shipping schedule!",
+                      "💡 We love hearing your suggestions!",
+                      "🚚 We provide detailed shipping statuses for each order!",
+                      "💵 We accept a wide variety of payment methods!",
+                      "👟 All pairs shown are available!"]
 
   var chosenFact = solenetFacts[Math.floor(Math.random()*solenetFacts.length)];
   console.log("Fact Displaying:" + chosenFact)   

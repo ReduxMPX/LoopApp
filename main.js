b7cc6bad0c25dfc42ce620704585997b60b1b92c
ReduxMPX <ReduxMPX@gmail.com>
2022-02-01 22:47:57 -0800

    test

--- main.js
+++ main.js
@@ -17,15 +17,3 @@ function eventCheck() {
   }
 
 
-  $(document).ready(function(){
-	// menu click event
-	$('.menuBtn').click(function() {
-		$(this).toggleClass('act');
-			if($(this).hasClass('act')) {
-				$('.mainMenu').addClass('act');
-			}
-			else {
-				$('.mainMenu').removeClass('act');
-			}
-	});
-});

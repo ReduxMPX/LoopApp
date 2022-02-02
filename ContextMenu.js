ff8ce781d2e340b674cd4bb16291af0a35c780c5
ReduxMPX <ReduxMPX@gmail.com>
2022-02-01 17:57:40 -0800

    tests

--- ContextMenu.js
+++ ContextMenu.js
@@ -119,79 +119,7 @@ class ContextMenu {
   }
 }
 
-
-const notifIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="none" stroke="currentColor" stroke-width="1.5" class="bi bi-bell-fill" viewBox="0 0 16 16">
-  <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z<circle cx="6" cy="6" r="3"></circle><circle cx="6" cy="18" r="3"></circle><line x1="20" y1="4" x2="8.12" y2="15.88"></line><line x1="14.47" y1="14.48" x2="20" y2="20"></line><line x1="8.12" y1="8.12" x2="12" y2="12"></line>"/>
-</svg>`;
-
-const cutIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="none" stroke="currentColor" stroke-width="1.5" class="bi bi-broadcast-pin" viewBox="0 0 16 16">
-  <path d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM6 8a2 2 0 1 1 2.5 1.937V15.5a.5.5 0 0 1-1 0V9.937A2 2 0 0 1 6 8z"/>
-</svg>`;
-
-const pasteIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" style="margin-right: 7px; position: relative; top: -1px" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>`;
-
-const downloadIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" style="margin-right: 7px; position: relative; top: -1px" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>`;
-
-const deleteIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" fill="none" style="margin-right: 7px" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>`;
-
-const menuItems = [
-  {
-    content: `${notifIcon}&nbsp;&nbsp;Soleware Fact`,
-    events: {
-      click: (e) => showFact()
-      // mouseover: () => console.log("Copy Button Mouseover")
-      // You can use any event listener from here
-    }
-  },
-  { content: `${pasteIcon}Paste` },
-  { content: `${cutIcon}Cut` },
-  { content: `${downloadIcon}Download` },
-  {
-    content: `${deleteIcon}Delete`,
-    divider: "top" // top, bottom, top-bottom
-  }
-];
-
-const light = new ContextMenu({
-  target: ".RDXTOPBAR",
-  mode: "light", // default: "dark"
-  menuItems
-});
-
-light.init();
-
-const dark = new ContextMenu({
-  target: ".RDXSECTIONHEADER",
-  menuItems
-});
-
-dark.init();
-
-// remove message
-
-function removeMessage() {
-  const message = document.querySelector(".right-click");
-  if (message) message.remove();
-}
-
-window.addEventListener("click", removeMessage);
-window.addEventListener("contextmenu", removeMessage);
-ContextMenu.js:46 Error: <path> attribute d: Expected path command, "….44-4.005-4.901z\u003Ccircle cx=".
-createItemMarkup @ ContextMenu.js:46
-(anonymous) @ ContextMenu.js:31
-getMenuItemsNode @ ContextMenu.js:30
-ContextMenu @ ContextMenu.js:7
-(anonymous) @ VM355:31
-ContextMenu.js:46 Error: <path> attribute d: Expected path command, "….44-4.005-4.901z\u003Ccircle cx=".
-createItemMarkup @ ContextMenu.js:46
-(anonymous) @ ContextMenu.js:31
-getMenuItemsNode @ ContextMenu.js:30
-ContextMenu @ ContextMenu.js:7
-(anonymous) @ VM355:39
-undefined
-const copyIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="none" stroke="currentColor" stroke-width="1.5" class="bi bi-bell-fill" viewBox="0 0 16 16">
-  <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z"/>
-</svg>`;
+const copyIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" style="margin-right: 7px" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>`;
 
 const cutIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" style="margin-right: 7px" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><circle cx="6" cy="6" r="3"></circle><circle cx="6" cy="18" r="3"></circle><line x1="20" y1="4" x2="8.12" y2="15.88"></line><line x1="14.47" y1="14.48" x2="20" y2="20"></line><line x1="8.12" y1="8.12" x2="12" y2="12"></line></svg>`;
 
@@ -203,9 +131,9 @@ const deleteIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="curr
 
 const menuItems = [
   {
-    content: `${copyIcon} Soleware Fact`,
+    content: `${copyIcon}Copy`,
     events: {
-      click: (e) => showFact()
+      click: (e) => console.log(e, "Copy Button Click")
       // mouseover: () => console.log("Copy Button Mouseover")
       // You can use any event listener from here
     }
@@ -243,3 +171,60 @@ function removeMessage() {
 
 window.addEventListener("click", removeMessage);
 window.addEventListener("contextmenu", removeMessage);
+
+// const notifIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="none" stroke="currentColor" stroke-width="1.5" class="bi bi-bell-fill" viewBox="0 0 16 16">
+//   <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z<circle cx="6" cy="6" r="3"></circle><circle cx="6" cy="18" r="3"></circle><line x1="20" y1="4" x2="8.12" y2="15.88"></line><line x1="14.47" y1="14.48" x2="20" y2="20"></line><line x1="8.12" y1="8.12" x2="12" y2="12"></line>"/>
+// </svg>`;
+
+// const cutIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="none" stroke="currentColor" stroke-width="1.5" class="bi bi-broadcast-pin" viewBox="0 0 16 16">
+//   <path d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707zm2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708zm5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708zm2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM6 8a2 2 0 1 1 2.5 1.937V15.5a.5.5 0 0 1-1 0V9.937A2 2 0 0 1 6 8z"/>
+// </svg>`;
+
+// const pasteIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" style="margin-right: 7px; position: relative; top: -1px" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>`;
+
+// const downloadIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" style="margin-right: 7px; position: relative; top: -1px" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>`;
+
+// const deleteIcon = `<svg viewBox="0 0 24 24" width="13" height="13" stroke="currentColor" stroke-width="2.5" fill="none" style="margin-right: 7px" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>`;
+
+// const menuItems = [
+//   {
+//     content: `${notifIcon}&nbsp;&nbsp;Soleware Fact`,
+//     events: {
+//       click: (e) => showFact()
+//       // mouseover: () => console.log("Copy Button Mouseover")
+//       // You can use any event listener from here
+//     }
+//   },
+//   { content: `${pasteIcon}Paste` },
+//   { content: `${cutIcon}Cut` },
+//   { content: `${downloadIcon}Download` },
+//   {
+//     content: `${deleteIcon}Delete`,
+//     divider: "top" // top, bottom, top-bottom
+//   }
+// ];
+
+// const light = new ContextMenu({
+//   target: ".RDXTOPBAR",
+//   mode: "light", // default: "dark"
+//   menuItems
+// });
+
+// light.init();
+
+// const dark = new ContextMenu({
+//   target: ".RDXSECTIONHEADER",
+//   menuItems
+// });
+
+// dark.init();
+
+// // remove message
+
+// function removeMessage() {
+//   const message = document.querySelector(".right-click");
+//   if (message) message.remove();
+// }
+
+// window.addEventListener("click", removeMessage);
+// window.addEventListener("contextmenu", removeMessage);

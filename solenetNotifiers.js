a5a7dbbd94c98e622cc2933526f8f02ea848f14e
ReduxMPX <ReduxMPX@gmail.com>
2022-02-08 22:52:46 -0800

    asd

--- solenetNotifiers.js
+++ solenetNotifiers.js
@@ -5,44 +5,27 @@
     fullWidth : "true",
 	})	 
 
-  let suggestIGWEB = new Toasted({
-    position: 'center',
-    theme: 'venice',
-    icon: 'announcement',
-    duration: '3500',
-    onComplete: () => {
-      console.log('User received IG suggestion')
-    }
-  })
 
-  const browserAgentRaw = navigator.userAgent;
-  if (browserAgentRaw.includes('Instagram')) { 
-    console.log("User on Instagram webkit")
-  }
-  else {
-    suggestIGWEB.show("We recommend using the Instagram browser instead for the best experience!")
-    console.warn("User not on Instagram webkit, sending Solenet browser change suggestion!")
-  }
-
-
-var intervalID = window.setInterval(showFact, 80000);
+  
+var intervalID = window.setInterval(showFact, 120000);
 
 
 function showFact() {
-    const solenetFacts = ["💜 SoleNet is run by one person!",
-    "📹 You should follow us on TikTok!",
-    "📱 SoleNet hosts livestreams!",
-    "📍 SoleNet is based in LA!",
-    "📊 Our analytics are publicly visible!",
-    "📱 Our site is built for our Instagram!",
-    "📦 Check out our shipping schedule!",
-    "💡 We love hearing your suggestions!",
-    "🚚 We provide detailed shipping statuses for each order!",
-    "💵 We accept a wide variety of payment methods!",
-    "👟 All pairs shown are available!",
-    "👍🏼 All of our reviews are on our Instagram!"]
+  const solenetFacts = ["💜 SoleNet is run by one person!",
+                      "📹 You should follow us on TikTok!",
+                      "📱 SoleNet hosts livestreams!",
+                      "📍 SoleNet is based in LA!",
+                      "📊 Our analytics are publicly visible!",
+                      "📱 Our site is built for our Instagram!",
+                      "📦 Check out our shipping schedule!",
+                      "💡 We love hearing your suggestions!",
+                      "🚚 We provide detailed shipping statuses for each order!",
+                      "💵 We accept a wide variety of payment methods!",
+                      "👟 All pairs shown are available!",
+                      "👍🏼 All of our reviews are on our Instagram!"]
 
-    var chosenFact = solenetFacts[Math.floor(Math.random()*solenetFacts.length)];
-    console.log("Fact Displaying:" + chosenFact)   
-    solenetFactNotif.show(chosenFact)
+  var chosenFact = solenetFacts[Math.floor(Math.random()*solenetFacts.length)];
+  console.log("Fact Displaying:" + chosenFact)   
+  solenetFactNotif.show(chosenFact)
 }
+
